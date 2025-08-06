965bfa87aa16d965bc15814d64a6add2674c1fb7
llylily <liyuanli.lily@gmail.com>
2025-08-06 21:29:47 +0200

    debug

--- CogModelingRNNsTutorial/rnn_utils.py
+++ CogModelingRNNsTutorial/rnn_utils.py
@@ -561,7 +561,7 @@ def fit_model(
     
     # first_loss = np.mean(losses['testing_loss'][:50])
 
-    test_loss_new = np.mean(losses['testing_loss'][:50])
+    test_loss_new = losses['testing_loss'][-1]
     all_losses += list(losses['testing_loss'])
 
     # 在第一个n_steps_per_call步骤后记录第一个损失
